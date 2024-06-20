766edf452c0a31f563473f15e10b2b818cd7d44d
S1coder <sg743810@gmail.com>
2024-06-20 16:27:05 +0530

    Files

---
+++ Variable.js
@@ -0,0 +1,4 @@
+//Syntax for variab le decalration --
+/*  ---       let variable_name:type = value;   ----         */
+var v = "Sachin Gupta";
+console.log(v);
---
+++ Variable.ts
@@ -0,0 +1,6 @@
+//Syntax for variab le decalration --
+
+/*  ---       let variable_name:type = value;   ----         */
+
+let v:string = "Sachin Gupta";
+console.log(v);
